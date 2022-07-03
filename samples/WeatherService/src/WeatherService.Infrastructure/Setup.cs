0bfd6991e0b437a0b2eaf613c87c05d4ab071a07
steffenskov <steffen@ckode.dk>
2022-07-03 15:38:14 +0200

    WIP

---
+++ samples/WeatherService/src/WeatherService.Infrastructure/Setup.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace WeatherService.Infrastructure;
+
+public static class Setup
+{
+	public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+	{
+		return services;
+	}
+}
