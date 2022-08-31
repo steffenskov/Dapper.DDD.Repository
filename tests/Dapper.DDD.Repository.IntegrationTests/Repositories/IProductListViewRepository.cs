b41de7856552fbbb65cbaf64f38a748b8f638788
steffenskov <steffen@ckode.dk>
2022-08-31 16:52:56 +0200

    WIP

--- tests/Dapper.DDD.Repository.IntegrationTests/Repositories/IProductListViewRepository.cs
+++ tests/Dapper.DDD.Repository.IntegrationTests/Repositories/IProductListViewRepository.cs
@@ -2,4 +2,6 @@ namespace Dapper.DDD.Repository.IntegrationTests.Repositories;
 public interface IProductListViewRepository : IViewRepository<ProductListView, int>
 {
 	Task<ProductListView?> GetByNameAsync(string name, CancellationToken cancellationToken = default);
+
+	Task<int?> GetProductIdByNameAsync(string name, CancellationToken cancellationToken = default);
 }
