8bae2aeece9c2ef882ced2f0efab8581d82ea84b
steffenskov <steffen@ckode.dk>
2022-09-10 19:49:52 +0200

    cleaned up code a bit

--- tests/Dapper.DDD.Repository.MySql.IntegrationTests/Startup.cs
+++ tests/Dapper.DDD.Repository.MySql.IntegrationTests/Startup.cs
@@ -8,8 +8,8 @@ public class Startup
 	public Startup()
 	{
 		var services = new ServiceCollection();
-		_ = services.AddOptions();
-		_ = services.ConfigureDapperRepositoryDefaults(options =>
+		services.AddOptions();
+		services.ConfigureDapperRepositoryDefaults(options =>
 		{
 			options.ConnectionFactory = new MySqlConnectionFactory(
 				"Server=localhost;Port=33060;Database=northwind;Uid=root;Pwd=mysql1337;AllowPublicKeyRetrieval=true;");
@@ -18,35 +18,35 @@ public class Startup
 			options.AddTypeConverter<CategoryId, int>(categoryId => categoryId.PrimitiveId, CategoryId.Create);
 			options.AddTypeConverter<Zipcode, int>(zipcode => zipcode.PrimitiveId, Zipcode.Create);
 		});
-		_ = services.AddTableRepository<Category, CategoryId>(options =>
+		services.AddTableRepository<Category, CategoryId>(options =>
 		{
 			options.TableName = "categories";
 			options.HasKey(x => x.CategoryID);
 			options.HasIdentity(x => x.CategoryID);
 		});
-		_ = services.AddTableRepository<CompositeUser, CompositeUserId>(options =>
+		services.AddTableRepository<CompositeUser, CompositeUserId>(options =>
 		{
 			options.TableName = "composite_users";
 			options.HasKey(x => x.Id);
 			options.HasDefault(x => x.DateCreated);
 		});
 
-		_ = services.AddViewRepository<ProductListView, int, IProductListViewRepository, ProductListViewRepository>(
+		services.AddViewRepository<ProductListView, int, IProductListViewRepository, ProductListViewRepository>(
 			options =>
 			{
 				options.ViewName = "current_product_list";
 				options.HasKey(x => x.ProductID);
 			});
-		_ = services.AddViewRepository<ProductListView>(options => { options.ViewName = "current_product_list"; });
+		services.AddViewRepository<ProductListView>(options => { options.ViewName = "current_product_list"; });
 
-		_ = services.AddTableRepository<Customer, Guid, ICustomerRepository, CustomerRepository>(options =>
+		services.AddTableRepository<Customer, Guid, ICustomerRepository, CustomerRepository>(options =>
 		{
 			options.TableName = "customers_with_value_object";
 			options.HasKey(x => x.Id);
 			options.Ignore(x => x.IdAndName);
 		});
 
-		_ = services.AddTableRepository<CustomerWithNestedAddresses, Guid>(options =>
+		services.AddTableRepository<CustomerWithNestedAddresses, Guid>(options =>
 		{
 			options.TableName = "customers_with_nested_value_object";
 			options.HasKey(x => x.Id);
--- tests/Dapper.DDD.Repository.Sql.IntegrationTests/Startup.cs
+++ tests/Dapper.DDD.Repository.Sql.IntegrationTests/Startup.cs
@@ -10,8 +10,8 @@ public class Startup
 	public Startup()
 	{
 		var services = new ServiceCollection();
-		_ = services.AddOptions();
-		_ = services.ConfigureDapperRepositoryDefaults(options =>
+		services.AddOptions();
+		services.ConfigureDapperRepositoryDefaults(options =>
 		{
 			options.ConnectionFactory = new SqlConnectionFactory(
 				"Server=127.0.0.1;Database=Northwind;User Id=sa;Password=SqlServerPassword#&%¤2019;Encrypt=False;");
@@ -29,28 +29,28 @@ public class Startup
 				geo => new SqlServerBytesWriter { IsGeography = false }.Write(geo),
 				bytes => (Polygon)new SqlServerBytesReader { IsGeography = false }.Read(bytes));
 		});
-		_ = services.AddTableRepository<Category, CategoryId>(options =>
+		services.AddTableRepository<Category, CategoryId>(options =>
 		{
 			options.TableName = "Categories";
 			options.HasKey(x => x.CategoryID);
 			options.HasIdentity(x => x.CategoryID);
 		});
-		_ = services.AddTableRepository<CompositeUser, CompositeUserId>(options =>
+		services.AddTableRepository<CompositeUser, CompositeUserId>(options =>
 		{
 			options.TableName = "CompositeUsers";
 			options.HasKey(x => x.Id);
 			options.HasDefault(x => x.DateCreated);
 		});
 
-		_ = services.AddViewRepository<ProductListView, int, IProductListViewRepository, ProductListViewRepository>(
+		services.AddViewRepository<ProductListView, int, IProductListViewRepository, ProductListViewRepository>(
 			options =>
 			{
 				options.ViewName = "[Current Product List]";
 				options.HasKey(x => x.ProductID);
 			});
-		_ = services.AddViewRepository<ProductListView>(options => { options.ViewName = "[Current Product List]"; });
+		services.AddViewRepository<ProductListView>(options => { options.ViewName = "[Current Product List]"; });
 
-		_ = services.AddTableRepository<Customer, Guid, ICustomerRepository, CustomerRepository>(options =>
+		services.AddTableRepository<Customer, Guid, ICustomerRepository, CustomerRepository>(options =>
 		{
 			options.TableName = "CustomersWithValueObject";
 			options.HasKey(x => x.Id);
