594e9c6ce2eebc14fddf6e37952525df9510d862
steffenskov <steffen@ckode.dk>
2024-05-13 15:41:13 +0200

    WIP

--- tests/Dapper.DDD.Repository.IntegrationTests/Aggregates/Customer.cs
+++ tests/Dapper.DDD.Repository.IntegrationTests/Aggregates/Customer.cs
@@ -4,7 +4,7 @@ namespace Dapper.DDD.Repository.IntegrationTests.Aggregates;
 
 public class Zipcode : StrongTypedId<Zipcode, int>
 {
-	public Zipcode(int primitiveId) : base(primitiveId)
+	public Zipcode(int primitiveValue) : base(primitiveValue)
 	{
 	}
 }
@@ -15,7 +15,7 @@ public record Customer
 	public string Name { get; init; } = default!;
 	public Address InvoiceAddress { get; init; } = default!;
 	public Address DeliveryAddress { get; init; } = default!;
-
+	
 	public string IdAndName =>
 		$"{Name} ({Id})"; // Only here to test .Ignore works correctly when configuring aggregates
 }
